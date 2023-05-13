f87d65f2cfcb3b838ee6595900f2ecf14fdf1d34
leonid-belikov <lifeisgym@gmail.com>
2023-05-13 20:14:10 +0300

    about-page: add portrait

---
+++ src/pages/about/index.test.ts
@@ -0,0 +1,12 @@
+import { renderApp } from '../../setup/test/renderApp'
+import { screen, within } from '@testing-library/react'
+import { ABOUT_PATH } from '../../router/routes'
+
+
+describe('About page component', () => {
+  it('should render the portrait', () => {
+    renderApp(ABOUT_PATH)
+    const aboutPage = screen.getByTestId('about-page')
+    expect(within(aboutPage).getByTestId('portrait')).toBeInTheDocument()
+  })
+})
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import styles from './style.module.scss'
+import portrait from '../../assets/images/portrait.svg'
+
 
 function AboutPage() {
   return (
-    <div data-testid='about-page'>
-      <h1>about-page</h1>
+    <div data-testid='about-page' className={styles['about-page']}>
+      <div data-testid='portrait' className={styles['portrait']}>
+        <img src={portrait} alt=''/>
+      </div>
     </div>
   )
 }
